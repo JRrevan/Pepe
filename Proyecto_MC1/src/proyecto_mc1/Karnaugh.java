93964ac97e5399f5d4d437384b80100486864f6e
Jonathan Rivera <riverajonathan332@gmail.com>
2025-04-17 17:07:23 -0600

    Karnaugh

--- Proyecto_MC1/src/proyecto_mc1/Karnaugh.java
+++ Proyecto_MC1/src/proyecto_mc1/Karnaugh.java
@@ -1,31 +1,26 @@
-package services;
+package proyecto_mc1;
 
-    import org.w3c.dom.*;
-    import javax.xml.parsers.DocumentBuilder;
-    import javax.xml.parsers.DocumentBuilderFactory;
-    import java.io.File;
-    import java.util.ArrayList;
-    import java.util.List;
-import javax.swing.JOptionPane;
+import org.w3c.dom.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Karnaugh {
-
+    
     public static class MapaKarnaugh {
         public int variables;
         public List<Integer> valores;
         public List<String> nombresVariables;
-
-
 
         public MapaKarnaugh(int variables, List<Integer> valores, List<String> nombresVariables) {
             this.variables = variables;
             this.valores = valores;
             this.nombresVariables = nombresVariables;
         }
-
     }
-
-
+    
     public MapaKarnaugh leerXML(String rutaArchivo) {
         try {
             File archivo = new File(rutaArchivo);
@@ -37,28 +32,24 @@ public class Karnaugh {
             Element mapa = (Element) doc.getElementsByTagName("mapa").item(0);
             int variables = Integer.parseInt(mapa.getAttribute("v"));
 
-            // Leer nombres de variables desde atributos val1, val2, ..., valN
             List<String> nombresVariables = new ArrayList<>();
             for (int i = 1; i <= variables; i++) {
                 String nombre = mapa.getAttribute("val" + i);
                 nombresVariables.add(nombre);
             }
 
-            // Leer valores del mapa
             NodeList valores = mapa.getElementsByTagName("valor");
             List<Integer> listaValores = new ArrayList<>();
             for (int i = 0; i < valores.getLength(); i++) {
                 Element valor = (Element) valores.item(i);
                 listaValores.add(Integer.parseInt(valor.getTextContent()));
             }
-
+            
             return new MapaKarnaugh(variables, listaValores, nombresVariables);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-
-
-
+    
 }
